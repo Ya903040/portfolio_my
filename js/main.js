1f013e1f5582b08f37c6fc4c03613e970bbd2b97
Suleiman <asirov560@gmail.com>
2022-10-28 14:32:53 +0600

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 let clicks = 0;
 
-//this is second
-const TIMEOUT = 10000;
 
 let display = document.querySelector('#display');
 const button = document.querySelector('#button');
@@ -12,7 +10,7 @@ button.onclick = start;
 
 function start() {
     button.onclick = () => counter.textContent = clicks++;
-    button.addEventListener("click",(where, element) =>{
+    button.addEventListener("click",() =>{
         if(clicks%2===0){
             let color = "#";
             color += Math.random().toString(16).slice(2,8);
