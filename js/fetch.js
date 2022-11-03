cb854a27a058037a640fb7909ac3035f4fdff1d5
Suleiman <asirov560@gmail.com>
2022-11-03 07:32:30 +0600

    refactoring

--- js/fetch.js
+++ js/fetch.js
@@ -7,14 +7,14 @@ function myWeatherClick() {
         .then(data => {
 
             let description_weather = ((data['weather'][0].description));
-            let temp = ((data['main']['temp']));
-            let feels_like = ((data['main']['feels_like']));
-            let temp_min = ((data['main']['temp_min']));
-            let temp_max = ((data['main']['temp_max']));
-            let pressure = ((data['main']['pressure']));
-            let humidity = ((data['main']['humidity']));
+            let temp = ((data['main']['temp'])) + " K";
+            let feels_like = ((data['main']['feels_like'])) + " K";
+            let temp_min = ((data['main']['temp_min'])) + " K";
+            let temp_max = ((data['main']['temp_max'])) + " K";
+            let pressure = ((data['main']['pressure'])) + " mbar";
+            let humidity = ((data['main']['humidity'])) + " %";
             let sea_level = ((data['main']['sea_level']));
-            let speed = ((data['wind']['speed']));
+            let speed = ((data['wind']['speed'])) + " m/s";
 
             document.getElementById('description_weather').innerHTML = description_weather;
             document.getElementById('temp').innerHTML = temp;
