ad78c547dab3bfbb6750bdf95dd09788ae4ac247
Suleiman <asirov560@gmail.com>
2022-11-04 15:11:03 +0600

    convert Kelvin to Celsius

--- js/fetch.js
+++ js/fetch.js
@@ -7,20 +7,24 @@ function myWeatherClick() {
         .then(data => {
 
             let description_weather = ((data['weather'][0].description));
-            let temp = ((data['main']['temp'])) + " K";
-            let feels_like = ((data['main']['feels_like'])) + " K";
-            let temp_min = ((data['main']['temp_min'])) + " K";
-            let temp_max = ((data['main']['temp_max'])) + " K";
+            let temp = ((data['main']['temp']) - 273.15);
+            let feels_like = ((data['main']['feels_like']) - 273.15);
+            let temp_min = ((data['main']['temp_min']) - 273.15);
+            let temp_max = ((data['main']['temp_max']) - 273.15);
             let pressure = ((data['main']['pressure'])) + " mbar";
             let humidity = ((data['main']['humidity'])) + " %";
             let sea_level = ((data['main']['sea_level']));
             let speed = ((data['wind']['speed'])) + " m/s";
 
+            let temp_convert_to_celsius = Math.round(temp * 100) / 100;
+            let feels_like_convert_to_celsius = Math.round(feels_like * 100) / 100;
+            let temp_min_convert_to_celsius = Math.round(temp_min * 100) / 100;
+            let temp_max_convert_to_celsius = Math.round(temp_max * 100) / 100;
             document.getElementById('description_weather').innerHTML = description_weather;
-            document.getElementById('temp').innerHTML = temp;
-            document.getElementById('feels_like').innerHTML = feels_like;
-            document.getElementById('temp_min').innerHTML = temp_min;
-            document.getElementById('temp_max').innerHTML = temp_max;
+            document.getElementById('temp').innerHTML = temp_convert_to_celsius + " C";
+            document.getElementById('feels_like').innerHTML = feels_like_convert_to_celsius + " C";
+            document.getElementById('temp_min').innerHTML = temp_min_convert_to_celsius + " C";
+            document.getElementById('temp_max').innerHTML = temp_max_convert_to_celsius + " C";
             document.getElementById('pressure').innerHTML = pressure;
             document.getElementById('humidity').innerHTML = humidity;
             document.getElementById('sea_level').innerHTML = sea_level;
